dbfb6bb6f4ce3d92c770dd28c1fc3e1f00603d17
Tommy Falgout <tfalgout@yahoo-inc.com>
2012-12-03 23:27:46 -0600

    Namespaced stuff.  Hopefully I got it right

--- js/dummyData.js
+++ js/dummyData.js
@@ -1,112 +1,120 @@
-datafillCreateFields = function() {
-    $('#createPcName').val('PC #10');
-    $('#createPcClass').val('Fighter');
-    $('#createPcLevel').val(8);
-    $('#createPcMaxHp').val(200);
-    $('#createPcAc').val(23);
-    $('#createPcFortitude').val(19);
-    $('#createPcReflex').val(17);
-    $('#createPcWill').val(18);
-    $('#createNpcName').val('NPC #10');
-    $('#createNpcClass').val('Cleric');
-    $('#createNpcLevel').val(8);
-    $('#createNpcMaxHp').val(200);
-    $('#createNpcAc').val(23);
-    $('#createNpcFortitude').val(19);
-    $('#createNpcReflex').val(17);
-    $('#createNpcWill').val(18);
-    $('#createEncounterName').val('Encounter #10');
-}
 
-setDummyData = function() {
-    activeEncounterId = 4;
+window.dmtool.datafill = function(model) {
+    var self = this;
+    self.dmModel = model;
 
-    nextEncounterListId = 6;
-    encounterList = {
-        2 : {
-            'name' : 'Dicks of Doom',
-            'pcList' : [5,6],
-            'npcList' : [7], 
-        },
-        3 : {
-            'name' : 'Shadows over Miami',
-            'pcList' : [],
-            'npcList' : [8], 
-        },
-        4 : {
-            'name' : 'Mirrored ceiling',
-            'pcList' : [5,6],
-            'npcList' : [9], 
-        },
-        5 : {
-            'name' : 'Peanuts and Dancing',
-            'pcList' : [5],
-            'npcList' : [8], 
-        },
-    };
+    self.datafillCreateFields = function() {
+        $('#createPcName').val('PC #10');
+        $('#createPcClass').val('Fighter');
+        $('#createPcLevel').val(8);
+        $('#createPcMaxHp').val(200);
+        $('#createPcAc').val(23);
+        $('#createPcFortitude').val(19);
+        $('#createPcReflex').val(17);
+        $('#createPcWill').val(18);
+        $('#createNpcName').val('NPC #10');
+        $('#createNpcClass').val('Cleric');
+        $('#createNpcLevel').val(8);
+        $('#createNpcMaxHp').val(200);
+        $('#createNpcAc').val(23);
+        $('#createNpcFortitude').val(19);
+        $('#createNpcReflex').val(17);
+        $('#createNpcWill').val(18);
+        $('#createEncounterName').val('Encounter #10');
+    }
+
+    self.setDummyData = function(model) {
+        model.activeEncounterId = 4;
+
+        model.nextEncounterListId = 6;
+        model.encounterList = {
+            2 : {
+                'name' : 'Dicks of Doom',
+                'pcList' : [5,6],
+                'npcList' : [7], 
+            },
+            3 : {
+                'name' : 'Shadows over Miami',
+                'pcList' : [],
+                'npcList' : [8], 
+            },
+            4 : {
+                'name' : 'Mirrored ceiling',
+                'pcList' : [5,6],
+                'npcList' : [9], 
+            },
+            5 : {
+                'name' : 'Peanuts and Dancing',
+                'pcList' : [5],
+                'npcList' : [8], 
+            },
+        };
 
 
-    nextPcListId = 7;
-    pcList = {
-        5 : {
-            'name'       : 'Umine',
-            'currentHp'  : 10,
-            'maxHp'      : 100,
-            'initiative' : 13,
-            'ac'         : 26,
-            'fortitude'  : 18,
-            'reflex'     : 15,
-            'will'       : 14,
-            'imgUrl'     : 'img/fry.jpg'
-        },
-        6 : {
-            'name'       : 'Eldon',
-            'currentHp'  : 10,
-            'maxHp'      : 50,
-            'initiative' : 18,
-            'ac'         : 20,
-            'fortitude'  : 18,
-            'reflex'     : 19,
-            'will'       : 19,
-            'imgUrl'     : 'img/bender.png'
-        },
-    };
+        model.nextPcListId = 7;
+        model.pcList = {
+            5 : {
+                'name'       : 'Umine',
+                'currentHp'  : 10,
+                'maxHp'      : 100,
+                'initiative' : 13,
+                'ac'         : 26,
+                'fortitude'  : 18,
+                'reflex'     : 15,
+                'will'       : 14,
+                'imgUrl'     : 'img/fry.jpg'
+            },
+            6 : {
+                'name'       : 'Eldon',
+                'currentHp'  : 10,
+                'maxHp'      : 50,
+                'initiative' : 18,
+                'ac'         : 20,
+                'fortitude'  : 18,
+                'reflex'     : 19,
+                'will'       : 19,
+                'imgUrl'     : 'img/bender.png'
+            },
+        };
 
-    nextPcListId = 10;
-    npcList = {
-        7 : {
-            'name'       : 'Baddy #1',
-            'currentHp'  : 100,
-            'maxHp'      : 100,
-            'initiative' : 14,
-            'ac'         : 26,
-            'fortitude'  : 18,
-            'reflex'     : 15,
-            'will'       : 14,
-            'imgUrl'     : 'img/hypnotoad.jpg'
-        },
-        8 : {
-            'name'       : 'Baddy #2',
-            'currentHp'  : 50,
-            'maxHp'      : 50,
-            'initiative' : 12,
-            'ac'         : 20,
-            'fortitude'  : 18,
-            'reflex'     : 19,
-            'will'       : 19,
-            'imgUrl'     : 'img/nibbler.png'
-        },
-        9 : {
-            'name'       : 'Baddy #3',
-            'currentHp'  : 50,
-            'maxHp'      : 50,
-            'initiative' : 10,
-            'ac'         : 20,
-            'fortitude'  : 18,
-            'reflex'     : 19,
-            'will'       : 19,
-            'imgUrl'     : 'img/mom.jpg'
-        },
-    };
+        model.nextPcListId = 10;
+        model.npcList = {
+            7 : {
+                'name'       : 'Baddy #1',
+                'currentHp'  : 100,
+                'maxHp'      : 100,
+                'initiative' : 14,
+                'ac'         : 26,
+                'fortitude'  : 18,
+                'reflex'     : 15,
+                'will'       : 14,
+                'imgUrl'     : 'img/hypnotoad.jpg'
+            },
+            8 : {
+                'name'       : 'Baddy #2',
+                'currentHp'  : 50,
+                'maxHp'      : 50,
+                'initiative' : 12,
+                'ac'         : 20,
+                'fortitude'  : 18,
+                'reflex'     : 19,
+                'will'       : 19,
+                'imgUrl'     : 'img/nibbler.png'
+            },
+            9 : {
+                'name'       : 'Baddy #3',
+                'currentHp'  : 50,
+                'maxHp'      : 50,
+                'initiative' : 10,
+                'ac'         : 20,
+                'fortitude'  : 18,
+                'reflex'     : 19,
+                'will'       : 19,
+                'imgUrl'     : 'img/mom.jpg'
+            },
+        };
 
+    }
+
+    return self;
 }
--- js/my.js
+++ js/my.js
@@ -1,120 +1,148 @@
 initApp = function () {
-    setDummyData();
-    datafillCreateFields();
-    addPopupBindings();
-    activeCreatureList = getCreatureListFromEncounter(activeEncounterId, encounterList, pcList, npcList);
-    addCreaturesToEncounterList(activeCreatureList);
-    setEncounterName(encounterList[activeEncounterId]['name']);
+    var dmToolModel    = new dmtool.model();
+    var dmToolUi       = new dmtool.ui( dmToolModel );
+    var dmToolDatafill = new dmtool.datafill( dmToolModel );
+
+    dmToolDatafill.setDummyData(dmToolModel);
+    dmToolDatafill.datafillCreateFields();
+
+    dmToolUi.refreshEncounterList();
+    dmToolUi.addPopupBindings();
+    dmToolUi.setEncounterName(dmToolModel.encounterList[dmToolModel.activeEncounterId]['name']);
 }
 
-addUser = function (name, currentHp, maxHp, initiative, ac, fortitude, reflex, will, imgUrl) {
-    liString = '<li data-theme="c"><a href="#popupEditPcEncounter" data-rel="popup"><img src="' + imgUrl + '" /><h1>' + name + '</h1><p>HP:' + currentHp + '/' + maxHp + 'Init:' + initiative + '</p><span class="ui-li-count">AC: ' + ac + ' For: ' + fortitude + ' Ref: ' + reflex + ' Wil: ' + will + '</span></a><a href="#popupEditCreature" data-rel="popup"></a></li>';
-    $( '#sortable' ).append(liString).listview("refresh");
-}
+window.dmtool = {};
 
-setEncounterName = function(encounterName) {
-    $( '#headerEncounterName' ).text( encounterName );
-}
+window.dmtool.model = function() {
+    var self = this;
 
-getCreatureListFromEncounter = function(encounterId, encounterList, pcList, npcList) {
-    var creatureList = [];
- 
-    activePcList = encounterList[encounterId]['pcList'];
-    for (pcId in activePcList) {
-        creatureList.push(pcList[activePcList[pcId]]);
+    self.activeEncounterId = 0;
+    self.nextEncounterListId = 0;
+    self.encounterList = {};
+    self.nextPcListId = 0;
+    self.pcList = {};
+    self.nextPcListId = 0;
+    self.npcList = {};
+
+    self.setActiveCreatureList = function(creatureList) {
+        activeCreatureList = creatureList;
     }
-    activeNpcList = encounterList[encounterId]['npcList'];
-    for (npcId in activeNpcList) {
-        creatureList.push(npcList[activeNpcList[npcId]]);
+
+    self.getEncounterList = function() {
+        return encounterList;
     }
-    return creatureList;
-}
 
-sortByInitiative = function(a, b) {
-    return( b.initiative - a.initiative );
-}
-
-setActiveCreatureList = function(creatureList) {
-    activeCreatureList = creatureList;
-}
-
-getEncounterList = function() {
-    return encounterList;
-}
-
-
-addCreaturesToEncounterList = function(creatureList) { 
-    sortedCreatureList = creatureList.sort(sortByInitiative);
-    for ( var creatureId in sortedCreatureList ) {
-        addUser(creatureList[creatureId]['name'], creatureList[creatureId]['currentHp'], creatureList[creatureId]['maxHp'], creatureList[creatureId]['initiative'], creatureList[creatureId]['ac'], creatureList[creatureId]['fortitude'], creatureList[creatureId]['reflex'], creatureList[creatureId]['will'], creatureList[creatureId]['imgUrl']);
+    self.getCreatureListFromEncounter = function(encounterId, encounterList, pcList, npcList) {
+        var creatureList = [];
+     
+        var activePcList = encounterList[encounterId]['pcList'];
+        for (pcId in activePcList) {
+            creatureList.push(pcList[activePcList[pcId]]);
+        }
+        var activeNpcList = encounterList[encounterId]['npcList'];
+        for (npcId in activeNpcList) {
+            creatureList.push(npcList[activeNpcList[npcId]]);
+        }
+        return creatureList;
     }
-}
 
 
-datafillPopup = function(divId, dataRel, link, list) {
-    $(divId + ' ul').html('');
-    for ( id in list) {
-        $(divId + ' ul').append('<li><a href="' + link + '" data-position-to="window" class="ui-btn-left" data-rel="' + dataRel + '">' + list[id]['name'] + '</a></li>').listview('refresh');
+    self.createEncounter = function( name ) {
+        var encounterInfo = { 'name' : name, 'pcList' : [], 'npcList' : [] };
+        self.encounterList[ self.nextEncounterListId ] = encounterInfo;
+        self.nextEncounterListId++;
     }
+    return self;
 }
 
-submitCreateEncounter = function() {
-    $( "#popupCreateEncounter" ).popup( "close" );
-    var encounterName = $( '#createEncounterName' ).val();
-    createEncounter(encounterName);     
-}
+window.dmtool.ui = function( dmToolModel ) {
+    var self = this;
 
-submitCreatePc = function() {
-    $( "#popupCreatePc" ).popup( "close" );
-    var newPcInfo = { 
-        'name'       : $( '#createPcName' ).val(), 
-        'maxHp'      : $( '#createPcMaxHp' ).val(), 
-        'initiative' : 0, 
-        'ac'         : $( '#createPcAc' ).val(), 
-        'fortitude'  : $( '#createPcFortitude' ).val(),
-        'reflex'     : $( '#createPcReflex' ).val(), 
-        'will'       : $( '#createPcWill' ).val(), 
-        'imgUrl'     : 'img/leela.jpg'
+    self.dmModel = dmToolModel;
+
+    self.refreshEncounterList = function() {
+        activeCreatureList = dmToolModel.getCreatureListFromEncounter(self.dmModel.activeEncounterId, self.dmModel.encounterList, self.dmModel.pcList, self.dmModel.npcList);
+        self.addCreaturesToEncounterList(activeCreatureList);
+    }
+
+    self.addUser = function (name, currentHp, maxHp, initiative, ac, fortitude, reflex, will, imgUrl) {
+        liString = '<li data-theme="c"><a href="#popupEditPcEncounter" data-rel="popup"><img src="' + imgUrl + '" /><h1>' + name + '</h1><p>HP:' + currentHp + '/' + maxHp + 'Init:' + initiative + '</p><span class="ui-li-count">AC: ' + ac + ' For: ' + fortitude + ' Ref: ' + reflex + ' Wil: ' + will + '</span></a><a href="#popupEditCreature" data-rel="popup"></a></li>';
+        $( '#sortable' ).append(liString).listview("refresh");
     };
-    pcList[ nextPcListId ] = newPcInfo;
-    nextPcListId++;
+
+    self.setEncounterName = function(encounterName) {
+        $( '#headerEncounterName' ).text( encounterName );
+    }
+
+    // http://stackoverflow.com/questions/8399882/jquery-mobile-collapsible-expand-collapse-event
+    // http://jquerymobile.com/demos/1.2.0/docs/pages/popup/events.html
+    self.addPopupBindings = function() {
+        $('#popupEdit').on('popupbeforeposition', function () { self.datafillPopup('#popupEditPc', 'popup', '#popupEditCreature', self.dmModel.pcList) });
+        $('#popupEdit').on('popupbeforeposition', function () { self.datafillPopup('#popupEditNpc', 'popup', '#popupEditCreature', self.dmModel.npcList) });
+        $('#popupEdit').on('popupbeforeposition', function () { self.datafillPopup('#popupEditEncounter', 'popup', '#popupEditEncounter', self.dmModel.encounterList) });
+        $('#popupAdd').on('popupbeforeposition', function () { self.datafillPopup('#popupAddPc', 'dialog', 'addCreature.html', self.dmModel.pcList) });
+        $('#popupAdd').on('popupbeforeposition', function () { self.datafillPopup('#popupAddNpc', 'dialog', 'addCreature.html', self.dmModel.npcList) });
+        $('#popupAdd').on('popupbeforeposition', function () { self.datafillPopup('#popupAddEncounter', 'dialog', '#popupAddEncounter', self.dmModel.encounterList) });
+        $('#createPcButton').on('click', function () { self.submitCreatePc() });
+        $('#createNpcButton').on('click', function () { self.submitCreateNpc() });
+        $('#createEncounterButton').on('click', function () { self.submitCreateEncounter() });
+    }
+
+    self.datafillPopup = function(divId, dataRel, link, list) {
+        $(divId + ' ul').html('');
+        for ( id in list) {
+            $(divId + ' ul').append('<li><a href="' + link + '" data-position-to="window" class="ui-btn-left" data-rel="' + dataRel + '">' + list[id]['name'] + '</a></li>').listview('refresh');
+        }
+    }
+
+    self.submitCreateEncounter = function() {
+        $( "#popupCreateEncounter" ).popup( "close" );
+        var encounterName = $( '#createEncounterName' ).val();
+        self.dmModel.createEncounter(encounterName);     
+    }
+
+    self.submitCreatePc = function() {
+        $( "#popupCreatePc" ).popup( "close" );
+        var newPcInfo = { 
+            'name'       : $( '#createPcName' ).val(), 
+            'maxHp'      : $( '#createPcMaxHp' ).val(), 
+            'initiative' : 0, 
+            'ac'         : $( '#createPcAc' ).val(), 
+            'fortitude'  : $( '#createPcFortitude' ).val(),
+            'reflex'     : $( '#createPcReflex' ).val(), 
+            'will'       : $( '#createPcWill' ).val(), 
+            'imgUrl'     : 'img/leela.jpg'
+        };
+        self.dmModel.pcList[ self.dmModel.nextPcListId ] = newPcInfo;
+        self.dmModel.nextPcListId++;
+    }
+
+    self.submitCreateNpc = function() {
+        $( "#popupCreatePc" ).popup( "close" );
+        var newNpcInfo = { 
+            'name'       : $( '#createNpcName' ).val(), 
+            'maxHp'      : $( '#createNpcMaxHp' ).val(), 
+            'initiative' : 0, 
+            'ac'         : $( '#createNpcAc' ).val(), 
+            'fortitude'  : $( '#createNpcFortitude' ).val(),
+            'reflex'     : $( '#createNpcReflex' ).val(), 
+            'will'       : $( '#createNpcWill' ).val(), 
+            'imgUrl'     : 'img/nibbler.jpg'
+        };
+        self.dmModel.npcList[ self.dmModel.nextNpcListId ] = newNpcInfo;
+        self.dmModel.nextNpcListId++;
+    }
+
+    self.addCreaturesToEncounterList = function(creatureList) { 
+        sortedCreatureList = creatureList.sort(self.sortByInitiative);
+        for ( var creatureId in sortedCreatureList ) {
+            self.addUser(creatureList[creatureId]['name'], creatureList[creatureId]['currentHp'], creatureList[creatureId]['maxHp'], creatureList[creatureId]['initiative'], creatureList[creatureId]['ac'], creatureList[creatureId]['fortitude'], creatureList[creatureId]['reflex'], creatureList[creatureId]['will'], creatureList[creatureId]['imgUrl']);
+        }
+    }
+
+    self.sortByInitiative = function(a, b) {
+        return( b.initiative - a.initiative );
+    }
+    return self;
 }
 
-submitCreateNpc = function() {
-    $( "#popupCreatePc" ).popup( "close" );
-    var newNpcInfo = { 
-        'name'       : $( '#createNpcName' ).val(), 
-        'maxHp'      : $( '#createNpcMaxHp' ).val(), 
-        'initiative' : 0, 
-        'ac'         : $( '#createNpcAc' ).val(), 
-        'fortitude'  : $( '#createNpcFortitude' ).val(),
-        'reflex'     : $( '#createNpcReflex' ).val(), 
-        'will'       : $( '#createNpcWill' ).val(), 
-        'imgUrl'     : 'img/nibbler.jpg'
-    };
-    npcList[ nextNpcListId ] = newNpcInfo;
-    nextNpcListId++;
-}
-
-// http://stackoverflow.com/questions/8399882/jquery-mobile-collapsible-expand-collapse-event
-// http://jquerymobile.com/demos/1.2.0/docs/pages/popup/events.html
-addPopupBindings = function() {
-    $('#popupEdit').on('popupbeforeposition', function () { datafillPopup('#popupEditPc', 'popup', '#popupEditCreature', pcList) });
-    $('#popupEdit').on('popupbeforeposition', function () { datafillPopup('#popupEditNpc', 'popup', '#popupEditCreature', npcList) });
-    $('#popupEdit').on('popupbeforeposition', function () { datafillPopup('#popupEditEncounter', 'popup', '#popupEditEncounter', encounterList) });
-    $('#popupAdd').on('popupbeforeposition', function () { datafillPopup('#popupAddPc', 'dialog', 'addCreature.html', pcList) });
-    $('#popupAdd').on('popupbeforeposition', function () { datafillPopup('#popupAddNpc', 'dialog', 'addCreature.html', npcList) });
-    $('#popupAdd').on('popupbeforeposition', function () { datafillPopup('#popupAddEncounter', 'dialog', '#popupAddEncounter', encounterList) });
-    $('#createPcButton').on('click', function () { submitCreatePc() });
-    $('#createNpcButton').on('click', function () { submitCreateNpc() });
-    $('#createEncounterButton').on('click', function () { submitCreateEncounter() });
-}
-
-createPc = function( name, maxHp, initiative, ac, fortitude, reflex, will, imgUrl ) {
-}
-
-createEncounter = function( name ) {
-    var encounterInfo = { 'name' : name, 'pcList' : [], 'npcList' : [] };
-    encounterList[ nextEncounterListId ] = encounterInfo;
-    nextEncounterListId++;
-}
